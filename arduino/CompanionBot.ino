3616e2e997ab424b8454786d5d6836003b33ea6a
SwitchctiwS <l.t_jared_thibault@hotmail.com>
2017-11-30 10:00:25 -0500

    Got 2 US to work
    the bot now responds to US and Pixy input.
    
    Also, removed  A LOT of code that wasn't being used.
    
    todo:
    - implement the pushbutton stops
    - bluetooth power level
    - remove jitter
    - tweak params in *.h file

--- arduino/CompanionBot.ino
+++ arduino/CompanionBot.ino
@@ -14,7 +14,6 @@ int motorPin2a = 9;
 int motorPin2b = 5;
 
 // Ultrasonic sensors 
-int usCenterPin = 0; // Analog pin
 int usLeftEchoPin = 4;
 int usLeftTrigPin = 2;
 int usRightEchoPin = 7;
@@ -35,8 +34,6 @@ void setup() {
     pinMode(motorPin2a, OUTPUT);
     pinMode(motorPin2b, OUTPUT);
 
-    pinMode(ledPin, OUTPUT);
-      
     Serial.begin(9600);
     
     // DEBUG
@@ -47,21 +44,7 @@ void setup() {
 
 void loop() {
     blocks = pixy.getBlocks();
-    static int usCenterDistance = 0, usRightDistance = 0, usLeftDistance = 0; 
-    // Note: The Arduino and US have the SAME amount of ADC bits, so it's fairly inaccurate
-
-    /*
-    // Gets commands from Serial 
-    // FOR DEBUG.
-    if (Serial.available() > 0) {
-         incomingByte = Serial.read();
-    }
-    */
-
-    /*
-    // Reads US value from EZ-something
-    usCenterDistance = analogRead(usCenterPin);
-    */
+    static int usRightDistance = 0, usLeftDistance = 0; 
 
     // Reads value from HC-SR04 ultrasonic sensor
     usLeftDistance = usLeft.ping_cm();
@@ -189,50 +172,9 @@ void moveBot(int usLeftDistance, int usRightDistance) {
             }
         }
     }
-    /*
-    if ((!isBlockDetected) || (usCenterDistance < US_MIN_DIST) || (usLeftDistance < US_MIN_DIST) || (usRightDistance < US_MIN_DIST)) {
-        moveWheels(0, 0, 0, 0);
-
-        //DEBUG
-        Serial.println("Stopped");
-
-    } else if (usCenterDistance > US_MAX_DIST) {
-        if (pixy.blocks[0].x > PIXY_WINDOW_LOW && pixy.blocks[0].x < PIXY_WINDOW_HIGH)  {
-            if (obstaclePosition == -1) {
-                moveWheels(1, 1, 127, 255);
-
-                //DEBUG
-                Serial.println("USLeft");
-
-            } else if (obstaclePosition == 1) {
-                moveWheels(1, 1, 255, 127);
-
-                //DEBUG
-                Serial.println("USRight");
-
-            } else {
-                moveWheels(1, 1, 255, 255);
-
-                //debug
-                Serial.println("Forward");
-            }
-        } else if (pixy.blocks[0].x > PIXY_WINDOW_HIGH) {
-            moveWheels(1, 1, 127, 255);
-
-            //DEBUG
-            Serial.println("Left");
-
-        } else if (pixy.blocks[0].x < PIXY_WINDOW_LOW) {
-            moveWheels(1, 1, 255, 127);
-
-            //DEBUG
-            Serial.println("Right");
-
-        }
-    } 
-    */
 }
 
+// Counts to remove jitter
 bool usObstacleDetected(uint16_t usLeftDistance, uint16_t usRightDistance) {
     // returns:
     //  true -> good
@@ -254,6 +196,7 @@ bool usObstacleDetected(uint16_t usLeftDistance, uint16_t usRightDistance) {
     return isObstacleDetected;
 }
 
+// Counts to remove jitter
 int usObstaclePosition (uint16_t usLeftDistance, uint16_t usRightDistance) {
     // returns:
     //   0 -> person in center
@@ -265,107 +208,31 @@ int usObstaclePosition (uint16_t usLeftDistance, uint16_t usRightDistance) {
     static int middleCount = 0;
     static int usObstaclePosition = 0; 
     
-    if (usLeftDistance < (usRightDistance + US_LEFT_DIFFERENTIAL)) {
-        leftCount++;
-        rightCount = 0;
-        middleCount = 0;
-    } else if (usRightDistance < (usLeftDistance + US_RIGHT_DIFFERENTIAL)) {
-        rightCount++;
-        leftCount = 0;
-        middleCount = 0;
-    } else {
-        middleCount++;
-        leftCount = 0;
-        rightCount = 0;
-    }
+    // If person is closer than the max allowed value, then execute US code
+    if ((usLeftDistance < US_LEFT_MAX_DIST) || usRightDistance < US_RIGHT_MAX_DIST) {
+        // if person's US distance on one sensor + a difference value (hysteresis value) is less than other US sensor, then turn the other way
+        if ((usLeftDistance + US_LEFT_DIFFERENTIAL) < usRightDistance) {
+            leftCount++;
+            rightCount = 0;
+            middleCount = 0;
+        } else if ((usRightDistance + US_RIGHT_DIFFERENTIAL) < usLeftDistance) {
+            rightCount++;
+            leftCount = 0;
+            middleCount = 0;
+        } else {
+            middleCount++;
+            leftCount = 0;
+            rightCount = 0;
+        }
 
-    if (leftCount > MAX_COUNT) {
-        usObstaclePosition = -1; 
-    } else if (rightCount > MAX_COUNT) {
-        usObstaclePosition = 1;
-    } else if (middleCount > MAX_COUNT) {
-        usObstaclePosition = 0;
+        if (leftCount > MAX_COUNT) {
+            usObstaclePosition = -1; 
+        } else if (rightCount > MAX_COUNT) {
+            usObstaclePosition = 1;
+        } else if (middleCount > MAX_COUNT) {
+            usObstaclePosition = 0;
+        }
     }
 
     return usObstaclePosition;
-}
-
-// Estimate based on area of CC
-uint16_t getPixyDist() {
-    // Use global Pixy variable
-    
-    // Make a lookup table:
-    // At xx distance CC is xxx pixels^2; at yy distance, CC is yyy pixels^2; at zzz...
-    // Do this for a bunch of values to determine the curve, then plot function accordingly.
-    // Use math function to make this function.
-    return 0; // dummy value to make compiler not kms
-}
-
-void acclerometerStuff() {
-    // Does something???
-}
-
-/* cosPhi
-|   x   |  y  |
-===============
-| 00.00 | 1.0 |
-| 25.84 | 0.9 |
-| 36.87 | 0.8 |
-| 45.57 | 0.7 |
-| 53.13 | 0.6 |
-| 60.00 | 0.5 |
-| 66.42 | 0.4 |
-| 72.54 | 0.3 |
-| 78.46 | 0.2 |
-| 84.26 | 0.1 |
-| 90.00 | 0.0 |
-===============*/
-float cosPhi(int16_t angle) {
-    float ratio;
-    bool negative = false;
-
-    angle %= 360;
-    if (angle < 0) {
-        angle += 360;
-    }
-
-    if (angle > 269) {
-        angle = 90 - (angle - 270);
-    } else if (angle > 179) {
-        angle -= 180;
-        negative = true;
-    } else if (angle > 89) { 
-        angle = 90 - (angle - 90);
-        negative = true;
-    }
-
-    if (angle < 26) {
-        ratio = 1.0;
-    } else if (angle < 37) {
-        ratio = 0.9;
-    } else if (angle < 46) {
-        ratio = 0.8;
-    } else if (angle < 53) {
-        ratio = 0.7;
-    } else if (angle < 60) {
-        ratio = 0.6;
-    } else if (angle < 66) {
-        ratio = 0.5;
-    } else if (angle < 73) {
-        ratio = 0.4;
-    } else if (angle < 78) {
-        ratio = 0.3;
-    } else if (angle < 84) {
-        ratio = 0.2;
-    } else if (angle < 90) {
-        ratio = 0.1;
-    } else { // angle == 90
-        ratio = 0.0;
-    }
-    
-    if (negative && ratio != 0.0) {
-        ratio *= -1.0;
-    }
-    
-    return ratio;
 }
